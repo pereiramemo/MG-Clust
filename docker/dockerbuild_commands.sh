5886e1180c69443f809b6b13ad367c2de7fd8b17
pereiramemo <pereiramemo@gmail.com>
2026-07-23 09:12:43 -0300

    corrected ghcr username

--- docker/dockerbuild_commands.sh
+++ docker/dockerbuild_commands.sh
@@ -1,8 +1,8 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-1 -t ghcr.io/epereira/mg-clust/module-1:latest .
-docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-2 -t ghcr.io/epereira/mg-clust/module-2:latest .
-docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-3 -t ghcr.io/epereira/mg-clust/module-3:latest .
-docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-4 -t ghcr.io/epereira/mg-clust/module-4:latest .
-docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-5 -t ghcr.io/epereira/mg-clust/module-5:latest .
-docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-6 -t ghcr.io/epereira/mg-clust/module-6:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-1 -t ghcr.io/pereiramemo/mg-clust/module-1:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-2 -t ghcr.io/pereiramemo/mg-clust/module-2:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-3 -t ghcr.io/pereiramemo/mg-clust/module-3:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-4 -t ghcr.io/pereiramemo/mg-clust/module-4:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-5 -t ghcr.io/pereiramemo/mg-clust/module-5:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-6 -t ghcr.io/pereiramemo/mg-clust/module-6:latest .
